07fb002156dd574a62115928753f2c0101225848
LarsVerschoor <larsverschoor2@gmail.com>
2025-01-29 23:49:15 +0100

    Added webservice images to cards

--- src/Components/AddPhotoForm.jsx
+++ src/Components/AddPhotoForm.jsx
@@ -6,9 +6,9 @@ function AddPhotoForm() {
 
     const [formData, setFormData] = useState({
         title: '',
-        name: '',
         description: '',
-        distance: ''
+        distance: '',
+        image: null
     });
 
     const [pendingResponse, setPendingResponse] = useState(false);
@@ -19,17 +19,26 @@ function AddPhotoForm() {
         setFormData({...formData, [name]: value});
     }
 
+    const handleFileChange = (e) => {
+        setFormData({...formData, image: e.target.files[0]});
+    }
+
     const handleSubmit = async (e) => {
         try {
             e.preventDefault();
             setPendingResponse(true);
+
+            const newPhoto = new FormData();
+            newPhoto.append('title', formData.title);
+            newPhoto.append('description', formData.description);
+            newPhoto.append('distance', formData.distance);
+            newPhoto.append('image', formData.image);
             const response = await fetch('http://145.24.223.230:8000/photos/', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',
                     'Accept': 'application/json'
                 },
-                body: JSON.stringify(formData)
+                body: newPhoto
             });
 
             if (response.status !== 201) {
@@ -58,21 +67,26 @@ function AddPhotoForm() {
                 }
                 <div className="flex flex-col gap-1">
                     <label htmlFor="title" className="font-bold">Title</label>
-                    <input type="text" name="title" id="title" value={formData.title} onChange={handleInputChange} className="border-2 border-gray-400 rounded py-2 px-4"/>
-                </div>
-                <div className="flex flex-col gap-1">
-                    <label htmlFor="name" className="font-bold">Name</label>
-                    <input type="text" name="name" id="name" value={formData.name} onChange={handleInputChange} className="border-2 border-gray-400 rounded py-2 px-4"/>
+                    <input type="text" name="title" id="title" value={formData.title} onChange={handleInputChange}
+                           className="border-2 border-gray-400 rounded py-2 px-4"/>
                 </div>
                 <div className="flex flex-col gap-1">
                     <label htmlFor="description" className="font-bold">Description</label>
-                    <textarea name="description" id="description" rows="3" value={formData.description} onChange={handleInputChange} className="border-2 border-gray-400 rounded py-2 px-4 resize-y"></textarea>
+                    <textarea name="description" id="description" rows="3" value={formData.description}
+                              onChange={handleInputChange}
+                              className="border-2 border-gray-400 rounded py-2 px-4 resize-y"></textarea>
                 </div>
                 <div className="flex flex-col gap-1">
                     <label htmlFor="distance" className="font-bold">Distance (light years)</label>
-                    <input type="number" name="distance" id="distance" value={formData.distance} onChange={handleInputChange} className="border-2 border-gray-400 rounded py-2 px-4"/>
+                    <input type="number" name="distance" id="distance" value={formData.distance}
+                           onChange={handleInputChange} className="border-2 border-gray-400 rounded py-2 px-4"/>
                 </div>
-                <input type="submit" value="Save photo" className="bg-black w-full block text-center text-white py-4 hover:bg-gray-800 cursor-pointer rounded"/>
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="image" className="font-bold">Image</label>
+                    <input type="file" name="image" id="image" onChange={handleFileChange} className="border-2 border-gray-400 rounded py-2 px-4"/>
+                </div>
+                <input type="submit" value="Save photo"
+                       className="bg-black w-full block text-center text-white py-4 hover:bg-gray-800 cursor-pointer rounded"/>
             </form>
         )
     );
--- src/Components/PhotoCard.jsx
+++ src/Components/PhotoCard.jsx
@@ -3,10 +3,12 @@ import PhotoDetailButton from "./PhotoDetailButton.jsx";
 
 function PhotoCard({photo}) {
     return (
-        <article className="shadow-lg shadow-gray-300 flex flex-col gap-3 p-3 rounded">
-            <SubHeader>{photo.title}</SubHeader>
-            <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" className="w-full rounded" alt=""/>
-            <PhotoDetailButton photoId={photo.id}/>
+        <article>
+            <article className="shadow-lg shadow-gray-300 bg-white flex flex-col gap-3 p-3 rounded">
+                <SubHeader>{photo.title}</SubHeader>
+                <img src={photo.imageUrl} className="w-full rounded object-contain" alt=""/>
+                <PhotoDetailButton photoId={photo.id}/>
+            </article>
         </article>
     );
 }
