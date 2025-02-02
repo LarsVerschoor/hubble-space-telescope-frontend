789dac3fabd4cc004039e2b2256dda5b1708d236
LarsVerschoor <larsverschoor2@gmail.com>
2025-02-02 14:12:04 +0100

    Removed unused code

--- src/Components/PhotoList.jsx
+++ src/Components/PhotoList.jsx
@@ -1,33 +1,9 @@
 import PhotoCard from "./PhotoCard.jsx";
-import {useSearchParams} from "react-router";
 import Pagination from "./Pagination.jsx";
 import {usePhotosContext} from "../Contexts/PhotosContext.jsx";
 
 function PhotoList() {
-    const [searchParams] = useSearchParams();
-
-    const {photos, setPhotos} = usePhotosContext()
-
-    const pageParsed = parseInt(searchParams.get('page'));
-    const limitParsed = parseInt(searchParams.get('limit'));
-    const page = typeof pageParsed === 'number' && !isNaN(pageParsed) ? pageParsed : 1;
-    const limit = typeof limitParsed === 'number' && !isNaN(limitParsed) ? limitParsed : 6;
-
-    const handleDelete = async (id) => {
-        try {
-            const photo = photos.find(photo => photo.id === id);
-            const response = await fetch(photo._links.self.href, {
-                method: 'DELETE',
-                headers: {
-                    'Accept': 'application/json'
-                }
-            });
-            if (response.status !== 204) throw new Error(`Failed to delete photo: ${response.status}, ${response.statusText}`);
-            setPhotos(prevPhotos => prevPhotos.filter(photo => photo.id !== id));
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    const {photos} = usePhotosContext()
 
     return (
         <section className="my-8">
